acac5c5d89e84d8f24d943206ffda6b05475a560
evan-cagiannos <99355231+evan-cagiannos@users.noreply.github.com>
2022-03-28 17:23:10 +0000

    Added js & css

---
+++ js/script.js
@@ -0,0 +1,18 @@
+// Copyright (c) 2020 Evan Cagiannos All rights reserved
+//
+// Created by: Evan Cagiannos
+// Created on: Sep 2020
+// This file contains the JS functions for index.html
+
+/**
+ * This function gets users first name and age and shows it back to user.
+ */
+function enterClicked() {
+  // input
+  const firstName = document.getElementById("first-name").value
+  const userAge = parseInt(document.getElementById("age-entered").value)
+
+  // output
+  document.getElementById("address").innerHTML =
+    "Your info is: " + firstName + ", age " + userAge + "."
+}
